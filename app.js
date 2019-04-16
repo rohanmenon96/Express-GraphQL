d016f550e2e56f70ba881dda4965bf8a13bb5386
rohanmenon96 <rohanmenon.nmims@gmail.com>
2019-04-15 22:58:37 -0400

    Schema and basic cretion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Packages Imported
 const express = require('express')
-
+const graphqlHTTP = require('express-graphql')
 
 const app = express();
 const port = 3000;
@@ -8,6 +8,10 @@ const port = 3000;
 app.listen(port,()=>{
     console.log("App is listening at port ",port)
 })
+
+app.use("/graphql", graphqlHTTP({
+    
+}))
 
 app.get("/",async(req,res)=>{
     res.send("Hello!");
